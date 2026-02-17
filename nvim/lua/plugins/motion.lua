d746aba96ba80fed889cca8d16df6e517385c995
Bowei Chen <bwchen.dev@gmail.com>
2026-02-17 15:27:13 +0800

    Consolidate nvim-surround into motion.lua and add template literal surround
    Move nvim-surround config from separate surround.lua into motion.lua to group
    motion/editing plugins together. Add new "T" surround for template literals (${}).

--- nvim/lua/plugins/motion.lua
+++ nvim/lua/plugins/motion.lua
@@ -59,7 +59,28 @@ return {
       vim.g.wordmotion_prefix = "\\"
     end,
   },
-  -- {
-  --   "tpope/vim-rsi",
-  -- }
+  {
+    "kylechui/nvim-surround",
+    version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    opts = {
+      surrounds = {
+        ["g"] = {
+          add = function()
+            local config = require("nvim-surround.config")
+            local input = config.get_input("Type: ")
+
+            if input then
+              return { { input .. "<" }, { ">" } }
+            end
+          end,
+        },
+        ["T"] = {
+          add = function()
+            return { { "${" }, { "}" } }
+          end,
+        },
+      },
+    },
+  },
 }
